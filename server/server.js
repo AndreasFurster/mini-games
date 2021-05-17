cca3ac63ae26825ba5567aa159523523617363d0
Andreas Furster <andreas.furster@hotmail.com>
2021-05-17 14:57:58 +0200

    Use ws on server

--- server/server.js
+++ server/server.js
@@ -11,8 +11,7 @@ const io = new Server(server, {
     origin: ["http://localhost:8080", "https://mini-games.andreasfurster.nl", "https://admin.socket.io"],
     methods: ["GET", "POST"],
     credentials: true,
-  },
-  transports: ['websocket']
+  }
 });
 
 instrument(io, {
